9713d2b3e6171b443c5b475a584a2fba7323a59c
Anirudh V <anirudhv@anirudhs-macbook-pro.local>
2018-06-07 14:26:02 -0700

    Initial Commit

---
+++ 8ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8ball
+//
+//  Created by Anirudh V on 6/7/18.
+//  Copyright © 2018 Anirudh V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
