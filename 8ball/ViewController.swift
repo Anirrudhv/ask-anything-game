ba0d2ee8948206c8cea201b4ac4063b0e01cedc8
Anirudh V <anirudhv@Anirudhs-MacBook-Pro.local>
2018-06-07 14:54:29 -0700

    Shoot your questions

--- 8ball/ViewController.swift
+++ 8ball/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balls: UIImageView!
+    var ballnumber:Int = 0
+    let arr1 = ["ball1","ball2","ball3","ball4","ball5"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ask(_ sender: Any) {
+        ani()
+        
+    }
+    func ani()
+    {
+        ballnumber = Int(arc4random_uniform(5))
+        balls.image = UIImage(named: arr1[ballnumber])
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        ani()
+    }
 }
 
